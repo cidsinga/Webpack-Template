d46103466085eeb2b74315129834f942f39db999
Student <example@example.com>
2019-11-19 15:45:31 -0800

    add import and export of objects

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-// import $ from 'jquery';
+// import { Object } from './triangle.js';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
